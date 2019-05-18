3a837c76a410a7afbdc8fe53e59d3a9336086ba7
Gvidas Reistaitis <gvidasreistaitis4@gmail.com>
2019-05-18 04:50:50 +0300

    Started doing some design. Finished design for profile creation and profile show activities

--- app/src/main/java/com/example/gvidas/activities/ProfileActivity.java
+++ app/src/main/java/com/example/gvidas/activities/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.gvidas.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,19 +25,27 @@ public class ProfileActivity extends AppCompatActivity {
     TextView list;
     EditText profileName, profileAge, profileWeight, profileHeight;
 
-    // Button buttonSave, buttonLoad;
+     Button buttonSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        list = (TextView) findViewById(R.id.listProfile);
+        //list = (TextView) findViewById(R.id.listProfile);
         profileName = (EditText) findViewById(R.id.profileNameTextField);
         profileAge = (EditText) findViewById(R.id.profileAgeTextField);
         profileHeight = (EditText) findViewById(R.id.profileHeightTextField);
         profileWeight = (EditText) findViewById(R.id.profileWeightTextField);
-
+        buttonSave = (Button) findViewById(R.id.saveProfile);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addProfile();
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         //Back button
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -54,7 +63,7 @@ public class ProfileActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void addProfile(View view) {
+    public void addProfile() {
         Random randId = new Random();
         MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
         // int id = Integer.parseInt(random.getText().toString());
